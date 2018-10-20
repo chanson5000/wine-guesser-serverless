74be2782f397bc3d0b0c9aadab3199cd09c4b3cd
Cory <chanson5000@gmail.com>
2018-10-19 21:57:18 -0500

    Added props that work.

--- src/frontend/components/common/CheckboxGroup.js
+++ src/frontend/components/common/CheckboxGroup.js
@@ -4,42 +4,42 @@ import Form from 'react-bootstrap/lib/Form';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 
-const CheckboxGroup = ({
-                         label, controlId, options, values
-                       }) => {
-
-  const optionsMap = options.map((option) => (
-      <Form.Check
-          type='checkbox'
-          key={option.value}
-          // id={option.value}
-          label={option.label}
-          value={option.value}
-      />
+const CheckboxGroup = ({ label, name, options, values, onChange }) => {
+  const optionsMap = options.map(option => (
+    <Form.Check
+      type="checkbox"
+      key={option.value}
+      name={option.value}
+      checked={values[option.value]}
+      label={option.label}
+      onChange={onChange}
+    />
   ));
 
   return (
-      <Form.Group as={Row} className="text-left justify-content-center" controlId={controlId}>
-        <Form.Label as="legend" md={2} column>
-          {label}
-        </Form.Label>
-        <Col md={3}>
-          {optionsMap}
-        </Col>
-      </Form.Group>
+    <Form.Group
+      as={Row}
+      className="text-left justify-content-center"
+      controlId={name}
+    >
+      <Form.Label as="legend" md={2} column>
+        {label}
+      </Form.Label>
+      <Col md={3}>{optionsMap}</Col>
+    </Form.Group>
   );
 };
 
-
 CheckboxGroup.propTypes = {
   label: PropTypes.string.isRequired,
-  controlId: PropTypes.string.isRequired,
-  values: PropTypes.array.isRequired,
+  name: PropTypes.string.isRequired,
+  values: PropTypes.objectOf(PropTypes.bool).isRequired,
+  onChange: PropTypes.func.isRequired,
   options: PropTypes.arrayOf(
-      PropTypes.shape({
-        value: PropTypes.string,
-        label: PropTypes.string
-      })
+    PropTypes.shape({
+      value: PropTypes.string,
+      label: PropTypes.string
+    })
   ).isRequired
 };
 
--- src/frontend/components/common/SelectListGroup.js
+++ src/frontend/components/common/SelectListGroup.js
@@ -4,40 +4,42 @@ import Form from 'react-bootstrap/lib/Form';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 
-const SelectListGroup = ({
-                           label, controlId, options, value, onChange
-                         }) => {
-
-  const optionsMap = options.map((option) => (
-      <option key={option.value} value={option.value}>
-        {option.label}
-      </option>
+const SelectListGroup = ({ label, name, options, value, onChange }) => {
+  const optionsMap = options.map(option => (
+    <option key={option.value} value={option.value}>
+      {option.label}
+    </option>
   ));
 
   return (
-      <Form.Group as={Row} className="text-left justify-content-center" controlId={controlId}>
-        <Form.Label md={2} column>
-          {label}
-        </Form.Label>
-        <Col md={3}>
-          <Form.Control as="select" onChange={onChange}>
-            {optionsMap}
-          </Form.Control>
-        </Col>
-      </Form.Group>
+    <Form.Group
+      as={Row}
+      className="text-left justify-content-center"
+      value={value}
+      controlId={name}
+    >
+      <Form.Label md={2} column>
+        {label}
+      </Form.Label>
+      <Col md={3}>
+        <Form.Control as="select" name={name} onChange={onChange}>
+          {optionsMap}
+        </Form.Control>
+      </Col>
+    </Form.Group>
   );
 };
 
 SelectListGroup.propTypes = {
   label: PropTypes.string.isRequired,
-  controlId: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   options: PropTypes.arrayOf(
-      PropTypes.shape({
-        value: PropTypes.string,
-        label: PropTypes.string
-      })
+    PropTypes.shape({
+      value: PropTypes.string,
+      label: PropTypes.string
+    })
   ).isRequired
 };
 

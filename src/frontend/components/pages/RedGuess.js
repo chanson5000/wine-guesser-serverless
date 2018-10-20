7e8faf4143ceeb5dc849b6b5ea8481848782c3dd
Cory <chanson5000@gmail.com>
2018-10-19 21:59:14 -0500

    State now works.

--- src/frontend/components/pages/RedGuess.js
+++ src/frontend/components/pages/RedGuess.js
@@ -6,20 +6,78 @@ import Button from 'react-bootstrap/lib/Button';
 
 class RedGuess extends Component {
   state = {
-    color: '',
-    tannin: '',
-    acidity: '',
-    alcohol: '',
-    climate: ''
+    color: 'garnet',
+    condition: {
+      tart: false,
+      ripe: false,
+      overripe: false,
+      baked: false
+    },
+    type: {
+      red: false,
+      black: false,
+      blue: false
+    },
+    nonFruit: {
+      floral: false,
+      vegetalPyrazine: false,
+      vegetalTomato: false,
+      herbalTobacco: false,
+      herbalMint: false,
+      herbalThyme: false,
+      herbalTea: false,
+      herbalOregano: false,
+      herbalDried: false,
+      spicePepper: false,
+      spiceAnise: false,
+      spiceOther: false,
+      coffee: false,
+      cocoa: false,
+      game: false,
+      smoke: false,
+      balsamic: false,
+      carbonicMaceration: false,
+      volatileAcidity: false,
+      oxidization: false,
+      organic: false,
+      inorganic: false,
+      oak: false
+    },
+    tannin: 'moderateMinus',
+    acidity: 'moderateMinus',
+    alcohol: 'moderateMinus',
+    climate: 'moderateMinus'
   };
 
   onChange = e => {
-    console.log('Value: ' + e.target.value);
-    this.setState({[e.target.controlId]: e.target.value});
+    if (e.target.type === 'checkbox') {
+      this.setState({
+        [e.target.id]: {
+          ...this.state[e.target.id],
+          [e.target.name]: e.target.checked
+        }
+      });
+    } else {
+      this.setState({ [e.target.name]: e.target.value });
+    }
   };
 
+  // // Debugging
+  // componentDidUpdate() {
+  //   console.log(this.state);
+  // }
+
   render() {
-    const { color, tannin, acidity, alcohol, climate } = this.state;
+    const {
+      color,
+      condition,
+      type,
+      nonFruit,
+      tannin,
+      acidity,
+      alcohol,
+      climate
+    } = this.state;
 
     return (
       <div className="container text-center">
@@ -34,22 +92,25 @@ class RedGuess extends Component {
           />
 
           <CheckboxGroup
-            controlId="condition"
+            name="condition"
             label="Condition"
+            values={condition}
             onChange={this.onChange}
             options={RedWineFields.condition}
           />
 
           <CheckboxGroup
-            controlId="fruit"
+            name="type"
             label="Fruit Type"
+            values={type}
             onChange={this.onChange}
             options={RedWineFields.type}
           />
 
           <CheckboxGroup
-            controlId="nonFruit"
+            name="nonFruit"
             label="Non-Fruit"
+            values={nonFruit}
             onChange={this.onChange}
             options={RedWineFields.nonFruit}
           />
--- src/frontend/components/pages/WhiteGuess.js
+++ src/frontend/components/pages/WhiteGuess.js
@@ -6,65 +6,144 @@ import SelectListGroup from '../common/SelectListGroup';
 import CheckboxGroup from '../common/CheckboxGroup';
 
 class WhiteGuess extends Component {
+  state = {
+    color: 'straw',
+    condition: {
+      tart: false,
+      ripe: false,
+      overripe: false,
+      baked: false
+    },
+    type: {
+      applePear: false,
+      citrus: false,
+      stone: false,
+      tropical: false
+    },
+    nonFruit: {
+      fruitBlossoms: false,
+      redFlowers: false,
+      hay: false,
+      herbalFresh: false,
+      chive: false,
+      herbalDried: false,
+      herbalSage: false,
+      herbalTea: false,
+      vegetalPyrazine: false,
+      spice: false,
+      terpene: false,
+      wax: false,
+      soap: false,
+      oysterShell: false,
+      botrytis: false,
+      oxidative: false,
+      lees: false,
+      organic: false,
+      inorganic: false,
+      oak: false,
+      bitter: false
+    },
+    sweetness: 'dry',
+    acidity: 'moderateMinus',
+    alcohol: 'moderateMinus',
+    climate: 'cool'
+  };
+
+  onChange = e => {
+    if (e.target.type === 'checkbox') {
+      this.setState({
+        [e.target.id]: {
+          ...this.state[e.target.id],
+          [e.target.name]: e.target.checked
+        }
+      });
+    } else {
+      this.setState({ [e.target.name]: e.target.value });
+    }
+  };
+
+  // // Debugging
+  // componentDidUpdate() {
+  //   console.log(this.state);
+  // }
+
   render() {
-    const value = [];
+    const {
+      color,
+      condition,
+      type,
+      nonFruit,
+      sweetness,
+      acidity,
+      alcohol,
+      climate
+    } = this.state;
+
     return (
       <div className="container text-center">
         <h2 className="p-2">Select white wine characteristics.</h2>
         <Form>
           <SelectListGroup
-            controlId="color"
+            name="color"
             label="Color"
-            value={value}
+            value={color}
+            onChange={this.onChange}
             options={WhiteWineFields.color}
           />
 
           <CheckboxGroup
-            controlId="condition"
+            name="condition"
             label="Condition"
-            values={value}
+            values={condition}
+            onChange={this.onChange}
             options={WhiteWineFields.condition}
           />
 
           <CheckboxGroup
-            controlId="fruit"
+            name="type"
             label="Fruit Type"
-            values={value}
+            values={type}
+            onChange={this.onChange}
             options={WhiteWineFields.type}
           />
 
           <CheckboxGroup
-            controlId="nonFruit"
+            name="nonFruit"
             label="Non-Fruit"
-            values={value}
+            values={nonFruit}
+            onChange={this.onChange}
             options={WhiteWineFields.nonFruit}
           />
 
           <SelectListGroup
-            controlId="sweetness"
+            name="sweetness"
             label="Sweetness"
-            value={value}
+            value={sweetness}
+            onChange={this.onChange}
             options={WhiteWineFields.structure}
           />
 
           <SelectListGroup
-            controlId="acidity"
+            name="acidity"
             label="Acidity"
-            value={value}
+            value={acidity}
+            onChange={this.onChange}
             options={WhiteWineFields.structure}
           />
 
           <SelectListGroup
-            controlId="alcohol"
+            name="alcohol"
             label="Alcohol"
-            value={value}
+            value={alcohol}
+            onChange={this.onChange}
             options={WhiteWineFields.structure}
           />
 
           <SelectListGroup
-            controlId="climate"
+            name="climate"
             label="Climate"
-            value={value}
+            value={climate}
+            onChange={this.onChange}
             options={WhiteWineFields.structure}
           />
 

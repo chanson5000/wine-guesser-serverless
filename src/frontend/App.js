815eed3ed1440641c8a6cba3cbb0f6554d3f7ecc
Cory <chanson5000@gmail.com>
2018-10-19 21:58:41 -0500

    Import restructure.

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,22 +1,25 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import {Header, Landing, Footer, About, RedGuess, WhiteGuess} from './components/layout';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Header, Footer } from './components/layout';
+import { About, Landing, RedGuess, WhiteGuess } from './components/pages';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-        <Router>
-          <div className="App">
-            <Header/>
-            <Route exact path="/" component={Landing}/>
-            <Route exact path="/guess-red" component={RedGuess}/>
-            <Route exact path="/guess-white" component={WhiteGuess}/>
-            <Route exact path="/about" component={About}/>
-            <Footer/>
-          </div>
-        </Router>
+      <Router>
+        <div className="App">
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/guess-red" component={RedGuess} />
+            <Route exact path="/guess-white" component={WhiteGuess} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }

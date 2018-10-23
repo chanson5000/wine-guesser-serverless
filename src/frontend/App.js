7ae267a6f87ef799799afb6c9e190d5be9b04a20
Cory <chanson5000@gmail.com>
2018-10-22 22:57:31 -0500

    Added page for showing all red wines and red wine record component.

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -8,29 +8,36 @@ import {
   WhiteGuess,
   NotFound,
   NewRedWine,
-    NewWhiteWine
+  NewWhiteWine
 } from './components/pages';
+import ShowAllRedWines from './components/pages/ShowAllRedWines';
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Header />
-          <Switch>
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/wine/red/guess" component={RedGuess} />
-            <Route exact path="/wine/white/guess" component={WhiteGuess} />
-            <Route exact path="/wine/red/new" component={NewRedWine} />
-            <Route exact path="/wine/white/new" component={NewWhiteWine} />
-            <Route exact path="/about" component={About} />
-            <Route component={NotFound} />
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <Switch>
+              <Route exact path="/" component={Landing} />
+              <Route exact path="/wine/red/guess" component={RedGuess} />
+              <Route exact path="/wine/white/guess" component={WhiteGuess} />
+              <Route exact path="/wine/red/new" component={NewRedWine} />
+              <Route exact path="/wine/white/new" component={NewWhiteWine} />
+              <Route exact path="/wine/red/all" component={ShowAllRedWines} />
+              <Route exact path="/about" component={About} />
+              <Route component={NotFound} />
+            </Switch>
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }

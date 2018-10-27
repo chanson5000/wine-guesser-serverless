30941a3cca833dfb050b9a97269de48f4013c8cf
Cory Hanson <chanson5000@gmail.com>
2018-10-27 12:52:26 -0500

    Moved database specific code out of util.

--- src/backend/util.js
+++ src/backend/util.js
@@ -1,33 +1,3 @@
-export const makeWineQueryParams = (table, varietal, world = null) => {
-  if (world !== null) {
-    return {
-      TableName: table,
-      KeyConditionExpression: 'varietal = :v and world = :w',
-      ExpressionAttributeValues: {
-        ':v': varietal,
-        ':w': world
-      }
-    };
-  }
-  return {
-    TableName: table,
-    KeyConditionExpression: 'varietal = :v',
-    ExpressionAttributeValues: {
-      ':v': varietal
-    }
-  };
-};
-
-export const isNullOrUndef = object => object === null || object === undefined;
-
-export const tryParameter = (parameters, key) => {
-  if (isNullOrUndef(parameters) || isNullOrUndef(parameters[key])) {
-    return null;
-  }
-
-  return decodeURIComponent(parameters[key]);
-};
-
 export const makeValidWineObject = event => {
   const combinedParams = {
     ...event.queryStringParameters,

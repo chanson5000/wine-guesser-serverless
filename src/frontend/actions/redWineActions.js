7c1af39340210f62ae44753573f70c276f0585fb
Cory <chanson5000@gmail.com>
2018-10-26 20:35:35 -0500

    Temporary default api endpoint url.

--- src/frontend/actions/redWineActions.js
+++ src/frontend/actions/redWineActions.js
@@ -1,9 +1,15 @@
 import { GET_RED_WINES } from './types';
 import axios from 'axios';
 
+const apiEndpoint = "https://9vqlpxuyhl.execute-api.us-east-2.amazonaws.com/Prod/";
+
 export const getRedWines = () => async dispatch => {
+  // const res = await axios.get(
+  //   'https://yvlt1timcc.execute-api.us-east-2.amazonaws.com/develop/wines/red'
+  // );
+
   const res = await axios.get(
-    'https://yvlt1timcc.execute-api.us-east-2.amazonaws.com/develop/wines/red'
+      `${apiEndpoint}wines/red`
   );
 
   dispatch({

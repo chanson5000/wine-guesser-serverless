f8cd91a850d568da7d6508a577c746abefbf952a
Cory <chanson5000@gmail.com>
2018-10-27 16:08:11 -0500

    Using the webpack DefinePlugin to pull api endpoint url.

--- src/frontend/actions/redWineActions.js
+++ src/frontend/actions/redWineActions.js
@@ -1,16 +1,10 @@
 import { GET_RED_WINES } from './types';
 import axios from 'axios';
 
-const apiEndpoint = "https://9vqlpxuyhl.execute-api.us-east-2.amazonaws.com/Prod/";
+const apiEndpoint = WINE_GUESSER_API_URL;
 
 export const getRedWines = () => async dispatch => {
-  // const res = await axios.get(
-  //   'https://yvlt1timcc.execute-api.us-east-2.amazonaws.com/develop/wines/red'
-  // );
-
-  const res = await axios.get(
-      `${apiEndpoint}wines/red`
-  );
+  const res = await axios.get(`${apiEndpoint}wines/red`);
 
   dispatch({
     type: GET_RED_WINES,

8c4e5948889bdd8808546ba1eacef61a4e5a48dc
Cory <chanson5000@gmail.com>
2018-10-28 12:05:36 -0500

    Added temporary administration page.

---
+++ src/frontend/components/pages/Administration.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Administration extends Component {
+  render() {
+    return (
+      <div className="container text-center">
+        <h2 className="p-4">Wine administration.</h2>
+        <div className="row justify-content-center">
+          <div className="col-auto">
+            <p>
+              <Link to="/wines/red">All Red Wines</Link>
+            </p>
+            <p>
+              <Link to="/wine/red/new">Add red wine.</Link>
+            </p>
+          </div>
+          <div className="col-auto">
+            <p>
+              <Link to="/wines/white">All White Wines</Link>
+            </p>
+            <p>
+              <Link to="/wine/white/new">Add white wine.</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Administration;

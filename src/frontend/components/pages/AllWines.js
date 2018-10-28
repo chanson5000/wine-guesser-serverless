eafb834150098c7f7474632aa0ccd3ea092b78de
Cory <chanson5000@gmail.com>
2018-10-28 11:04:06 -0500

    Add page component for show of all red and white wines.

---
+++ src/frontend/components/pages/AllWines.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import WineRecord from '../wine/WineRecord';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Container } from 'react-bootstrap';
+import { getAllWines } from '../../actions/wineActions';
+
+class AllWines extends Component {
+  componentDidMount() {
+    this.props.getAllWines(this.props.isRedWine);
+  }
+
+  render() {
+    const { wines, isRedWine } = this.props;
+
+    return (
+      <Container>
+        <React.Fragment>
+          <h2 className="text-center p-4">
+            All {isRedWine ? 'Red' : 'White'} Wines
+          </h2>
+
+          {wines.map(wine => (
+            <WineRecord
+              isRedWine={isRedWine}
+              key={wine.varietal + wine.world}
+              wine={wine}
+            />
+          ))}
+        </React.Fragment>
+      </Container>
+    );
+  }
+}
+
+AllWines.propTypes = {
+  wines: PropTypes.array.isRequired,
+  getAllWines: PropTypes.func.isRequired,
+  isRedWine: PropTypes.bool.isRequired
+};
+
+AllWines.defaultProps = {
+  isRedWine: false
+};
+
+const mapStateToProps = state => ({
+  wines: state.wine.wines
+});
+
+export default connect(
+  mapStateToProps,
+  { getAllWines }
+)(AllWines);

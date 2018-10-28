8e63f0ba894ca9ecc34cee244f5238cd7a705332
Cory <chanson5000@gmail.com>
2018-10-28 12:06:31 -0500

    Added temporary administration link to landing.

--- src/frontend/components/pages/Landing.js
+++ src/frontend/components/pages/Landing.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import WineGlassLink from '../common/WineGlassLink';
 
 class Landing extends Component {
@@ -13,6 +14,9 @@ class Landing extends Component {
           <div className="col-auto">
             <WineGlassLink isRedWine={false} />
           </div>
+        </div>
+        <div>
+          <Link to="/administration">Administration</Link>
         </div>
       </div>
     );

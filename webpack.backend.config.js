923f7b1da46734cc5e3e681a97947e8b091129c3
Cory <chanson5000@gmail.com>
2019-09-21 15:00:01 -0500

    Updated backend config.

--- webpack.backend.config.js
+++ webpack.backend.config.js
@@ -3,24 +3,9 @@ const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   mode: 'production',
-  entry: ['@babel/polyfill', './src/backend/index.js'],
+  entry: './src/backend/index.js',
   externals: [
-    nodeExternals({
-      whitelist: [
-        '@babel/polyfill',
-        'core-js/es6',
-        'core-js/fn/array/includes',
-        'core-js/fn/string/pad-start',
-        'core-js/fn/string/pad-end',
-        'core-js/fn/symbol/async-iterator',
-        'core-js/fn/object/get-own-property-descriptors',
-        'core-js/fn/object/values',
-        'core-js/fn/object/entries',
-        'core-js/fn/promise/finally',
-        'core-js/web',
-        'regenerator-runtime/runtime'
-      ]
-    })
+    nodeExternals()
   ],
   module: {
     rules: [

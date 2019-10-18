0e236e3b93709cdcf5717f15ac9d0e921fda2021
Cory Hanson <chanson5000@gmail.com>
2019-10-17 20:12:19 -0500

    WIP

--- src/frontend/components/pages/AllWines.js
+++ src/frontend/components/pages/AllWines.js
@@ -8,58 +8,53 @@ import arrowSpinner from '../../components/common/arrowSpinner.gif';
 
 export default function AllWines({ isRedWine }) {
   const [wines, setWines] = useState([]);
-  const [error, setError] = useState({});
+  const [error, setError] = useState();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoading(true);
     loadWines();
   }, []);
 
   async function loadWines() {
+    setLoading(true);
     try {
       const response = await getAllWines(isRedWine);
       setWines(response);
     } catch (e) {
       setWines([]);
-      setError(e);
+      setError('Unable to load wines.');
     } finally {
       setLoading(false);
     }
   }
 
-  function onWineDeleted() {
-    loadWines();
-  }
-
   return (
     <Container>
-      <React.Fragment>
-        <h2 className="text-center p-4">
-          All {isRedWine && isRedWine ? 'Red' : 'White'} Wines
-        </h2>
-        {loading === true && (
-          <div className="text-center p-4">
-            <img src={arrowSpinner} alt="Loading..." />
-          </div>
-        )}
-        {wines &&
-          wines.map(wine => (
-            <Link
-              to={`/wines/${isRedWine === true ? 'red' : 'white'}/${
-                wine.varietal
-              }/${wine.world}`}
+      <h2 className="text-center p-4">
+        All {isRedWine && isRedWine ? 'Red' : 'White'} Wines
+      </h2>
+      {error && <p>${error}</p>}
+      {loading && (
+        <div className="text-center p-4">
+          <img src={arrowSpinner} alt="Loading..." />
+        </div>
+      )}
+      {wines &&
+        wines.map(wine => (
+          <Link
+            to={`/wines/${isRedWine === true ? 'red' : 'white'}/${
+              wine.varietal
+            }/${wine.world}`}
+            key={wine.varietal + wine.world}
+          >
+            <WineCard
+              isRedWine={isRedWine}
               key={wine.varietal + wine.world}
-            >
-              <WineCard
-                isRedWine={isRedWine}
-                key={wine.varietal + wine.world}
-                wine={wine}
-                onWineDeleted={onWineDeleted}
-              />
-            </Link>
-          ))}
-      </React.Fragment>
+              wine={wine}
+              onWineDeleted={loadWines}
+            />
+          </Link>
+        ))}
     </Container>
   );
 }
---
+++ test/unit/frontend/components/common/AllWines.spec.js
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import { getAllWines } from '../../../../../src/frontend/actions/WineRestService';
+
+jest.mock('../../../../../src/frontend/actions/WineRestService');
+
+test('AllWines component', () => {
+
+});
